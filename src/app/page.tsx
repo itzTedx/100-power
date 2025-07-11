0a0a9006f0bee194d979a0b5aa40b689f50f99e9
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-11 12:03:06 +0400

    Enhance homepage layout and content; add a section promoting 100 Power's performance benefits and include a call-to-action button linking to solutions.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main className="min-h-svh"></main>;
+  return (
+    <main className="min-h-svh">
+      <section className="flex min-h-svh items-end">
+        <div className="container py-20">
+          <div className="grid grid-cols-3 gap-6">
+            <h1 className="col-span-2 text-7xl font-bold tracking-tighter italic">
+              Unleash 100% Performance with <span>100 Power.</span>
+            </h1>
+            <p className="font-aloevera text-xl">
+              Next-gen lubrication technology to maximize performance, minimize
+              friction, and protect the planet.
+            </p>
+          </div>
+          <Button asChild>
+            <Link href="/solutions">Explore Solutions</Link>
+          </Button>
+        </div>
+      </section>
+    </main>
+  );
 }
