06dedbafa9f95078c6bd6242ae2c78d3e0de89b2
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-11 16:28:17 +0400

    Refactor Faqs component layout; enhance visual presentation with updated structure, add button for expert consultation, and improve styling for better user engagement.

--- src/components/sections/faqs.tsx
+++ src/components/sections/faqs.tsx
@@ -1,19 +1,34 @@
+import { IconArrowUpRight } from "@tabler/icons-react";
+
 import Beams from "../background/beams";
+import { Button } from "../ui/button";
 
 export const Faqs = () => {
   return (
     <section className="container py-12">
-      <div className="relative h-96">
-        <Beams
-          beamWidth={2}
-          beamHeight={30}
-          beamNumber={12}
-          lightColor="#EFBF04"
-          speed={2}
-          noiseIntensity={1.75}
-          scale={2}
-          rotation={30}
-        />
+      <div className="flex flex-col items-center">
+        <div className="relative grid h-80 w-full place-content-center overflow-hidden rounded-sm">
+          <div className="absolute inset-0">
+            <Beams
+              beamWidth={4}
+              beamHeight={30}
+              beamNumber={12}
+              lightColor="#EFBF04"
+              speed={2}
+              noiseIntensity={1.75}
+              scale={0.25}
+              rotation={30}
+            />
+          </div>
+          <h2 className="text-secondary-foreground relative z-50 text-4xl font-medium">
+            Have any question?
+          </h2>
+        </div>
+        <Button variant="secondary" className="-translate-y-1/2">
+          Talk to an expert <IconArrowUpRight />
+        </Button>
+
+        <h3 className="text-center text-3xl">Frequently Asked Questions</h3>
       </div>
     </section>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         outline:
           "border border-secondary bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 font-bold",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary shadow-sm",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
